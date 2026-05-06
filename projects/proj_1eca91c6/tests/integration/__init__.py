0d4e25401a256f6c7190b0f7f3caf663c624ab99
RUOK <781269758@qq.com>
2026-05-06 14:49:38 +0800

    chore: remove tracked generated project

--- projects/proj_1eca91c6/tests/integration/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Integration tests for Snake Game API.
-
-Tests the REST API endpoints end-to-end.
-"""
--- projects/proj_1eca91c6/tests/unit/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Unit tests for Game Studio components.
-
-This package contains unit tests for game logic, state machines,
-and other core components.
-"""
